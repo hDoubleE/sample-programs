829d49cf4e1f95e8c386dc630898ca4cf16d7cb2
hDoubleE <Trevor.Heehs@stmartin.edu>
2018-12-06 15:20:15 -0800

    added string folder

--- StringPlayground/StringPlaygroung.cs
+++ StringPlayground/StringPlaygroung.cs
@@ -16,7 +16,7 @@ namespace StringPlayground
         static void Main()
         {
             // Let's start by declaring some strings to play with.
-            string s1 = "My dear aunt Sally";
+            string s1 = "My dear Aunt Sally";
             string s2 = "It was a dark and stormy night";
             string s3 = "Lorem Ipsum is cool";
             string s4 = "I guess I need to know this stuff";
@@ -24,58 +24,75 @@ namespace StringPlayground
             // </Array Notation>
             // You can use array notation with an index value to acquire
             // read-only access to individual characters in a string.
-            Console.WriteLine("Array Notation");
-            Console.Write("Cool"[0]);
-            Console.WriteLine("Cool"[3]);         // Cl
-			Console.WriteLine(s1[4]);             // e
-			char yInS1 = s1[1];            
+            
+            // String literal as param
+            Console.Write("Boss"[0]);
+            Console.WriteLine("Boss"[3]);         // Bo
+			
+            // String variable as param
+            Console.WriteLine(s1[4]);             // e
+
+			// Store specified char in string to variable.
+            char yInS1 = s1[1];            
             Console.WriteLine(yInS1);             // y
+
+            // Specify char using String.Length.
 			Console.WriteLine(s2[s2.Length - 4]); // i
 			Console.WriteLine(s2[s2.Length - 1]); // t
-            Console.WriteLine();
             // <Array Notation/>
 
             // </String.Contains Method>
             // Returns a boolean value indicating whether 
             // a specified substring occurs within a string.
-            Console.WriteLine("Contains Method");
-            string s5 = "know";
-            bool s5inS4 = s4.Contains(s5);
-            Console.WriteLine(s5inS4);              // True
-            Console.WriteLine(s2.Contains("cool")); // False
-            System.Console.WriteLine();
+            
+            // String literal as param, store result in boolean variable.
+            bool wordInS4 = s4.Contains("know");
+            Console.WriteLine(wordInS4);           // True
+
+            // String variable as param 
+            string word = "Lorem";
+            Console.WriteLine(s4.Contains(word)); // False
             // <String.Contains Method/>
 
-            //</StartsEndsWith>
-            // Determines whether the beginning of
+            // </StartsWith and EndsWith>
+            // Determines whether the beginning of a
             // string instance matches a specified string.
             // Returns a boolean if string parameter matches 
             // string being searched. 
-            Console.WriteLine("StartsWith and EndsWith Method");
+            
+            // String literals as params to search for.
+            // Case sensitive.
             Console.WriteLine(s2.StartsWith("It"));    // True
             Console.WriteLine(s2.StartsWith("it"));    // False
             Console.WriteLine(s2.EndssWith("night.")); // True
             Console.WriteLine(s2.EndssWith("Night.")); // False
-            //<StartsEndsWith/>
+            // <StartsWith and EndsWith/>
 
-            // </IndexOf LastIndexOf>
-            // Reports the index of the first occurrence of a character or string within instance. 
+            // </IndexOf and LastIndexOf>
+            // Returns the index position as int of the first occurrence 
+            // of a character or string within instance. 
             // The method returns -1 if character or string is not found.
-            Console.WriteLine("IndexOf and LastIndexOf Method");
-            Console.WriteLine(s2.IndexOf("I"));     // 0
+            
+            // Accepts word(string literal) or individual char as param.
+            Console.WriteLine(s2.IndexOf("It"));    // 0
             Console.WriteLine(s2.IndexOf("y"));     // 23
-            Console.WriteLine(s2.LastIndexOf("i")); // 26
+
+            // If param is a word, returns first char index.
+            Console.WriteLine(s2.IndexOf("dark"));  // 9
+            
+            // Find first and last instance of char in string instance.
             Console.WriteLine(s2.IndexOf("a"));     // 4
             Console.WriteLine(s2.LastIndexOf("a")); // 14
+
+            // Return -1 if char is not found.
             Console.WriteLine(s2.IndexOf("b"));     // -1
-            Console.WriteLine(s2.IndexOf("c"));     // -1
-            // <IndexOf LastIndexOf/>
+            // <IndexOf and LastIndexOf/>
 
             // </Compare and IndexOf>
             // Let's combine the Contains and IndexOf Methods in a quick program. 
             Console.WriteLine("Contains and IndexOf Methods Combined");
             
-            string searchFor = "aunt";
+            string searchFor = "Aunt";
             bool b = s1.Contains(searchFor);
             Console.WriteLine($"{searchFor} is in the string {s1}: {b}");
            
@@ -95,13 +112,12 @@ namespace StringPlayground
             // Use IndexOf to declare a couple of placeholders
             int d = s2.IndexOf("d");
             int aunt = s1.IndexOf("r");
-            // Play with Substring to assemble a custom string.
+            // Use Substrings to assemble a custom string from three source string.
             Console.Write(s1.Substring(0, 3));
             Console.Write(s2.Substring(d, 15));
             Console.Write(s1.Substring(aunt + 1));
             Console.WriteLine(s3.Substring(11));
-
-            // Output: "My dark and stormy aunt Sally is cool"
+            // Output: "My dark and stormy Aunt Sally is cool"
 
             //<Substring/>
 
@@ -110,13 +126,13 @@ namespace StringPlayground
             // is inserted at a specified index position in this instance.
 
             Console.WriteLine(s1.Insert(7, "old and crazy "));
-            // Output: My dear old crazy aunt Sally.
+            // Output: My dear old crazy Aunt Sally.
 
             Console.WriteLine(s4.Insert(28, s2.Substring(9) + " "));
             // Output: "I guess I need to know this dark and stormy night stuff"
 
             Console.WriteLine(s1.Remove(2, 5));
-            //Output: "My aunt Sally"
+            //Output: "My Aunt Sally"
 
             Console.WriteLine(s4.Remove(23).Insert(22, "...about strings!"));
             // I guess I need to know...about strings!
@@ -134,7 +150,7 @@ namespace StringPlayground
 
             // Second param takes a char and inserts it into extra space.
             Console.WriteLine(s1.PadLeft(s2.Length + 5, '.'));
-            // Output: ".....My dear aunt Sally"
+            // Output: ".....My dear Aunt Sally"
             
             // PadRight computes the same at the end of string.
             Console.WriteLine(s2.PadRight(s2.Length + 15, '!'));
@@ -143,7 +159,7 @@ namespace StringPlayground
             // Place char in variable and pass as param. 
             char pad = '.';
             Console.WriteLine(s1.PadRight(s1.Length + 5, pad));
-            // "Output: My dear aunt Sally....."
+            // "Output: My dear Aunt Sally....."
             //<PadLeft and PadRight/>
 
             //</Trim Methods>
