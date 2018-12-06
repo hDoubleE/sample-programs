991b78403b1af1315a5c131d30d017c2348d083d
hDoubleE <Trevor.Heehs@stmartin.edu>
2018-12-05 17:03:12 -0800

    changed strings

--- StringPlaygroung.cs
+++ StringPlaygroung.cs
@@ -25,6 +25,7 @@ namespace StringPlayground
             // </Array Notation>
             // You can use array notation with an index value to acquire
             // read-only access to individual characters in a string.
+            Console.WriteLine("Array Notation");
             Console.Write("Cool"[0]);
             Console.WriteLine("Cool"[3]);         // Cl
 			Console.WriteLine(s1[4]);             // e
@@ -32,20 +33,62 @@ namespace StringPlayground
             Console.WriteLine(yInS1);             // y
 			Console.WriteLine(s2[s2.Length - 4]); // i
 			Console.WriteLine(s2[s2.Length - 1]); // t
+            Console.WriteLine();
             // <Array Notation/>
 
             // </String.Contains Method>
             // Returns a boolean value indicating whether 
             // a specified substring occurs within a string.
+            Console.WriteLine("Contains Method");
             string s5 = "know";
             bool s5inS4 = s4.Contains(s5);
             Console.WriteLine(s5inS4);              // True
             Console.WriteLine(s2.Contains("cool")); // False
-
+            System.Console.WriteLine();
             // <String.Contains Method/>
 
+            //</StartsEndsWith>
+            // Determines whether the beginning of
+            // string instance matches a specified string.
+            // Returns a boolean if string parameter matches 
+            // string being searched. 
+            Console.WriteLine("StartsWith and EndsWith Method");
+            Console.WriteLine(s2.StartsWith("It"));    // True
+            Console.WriteLine(s2.StartsWith("it"));    // False
+            Console.WriteLine(s2.EndssWith("night.")); // True
+            Console.WriteLine(s2.EndssWith("Night.")); // False
+            //<StartsEndsWith/>
 
+            // </IndexOf LastIndexOf>
+            // Reports the index of the first occurrence of a character or string within instance. 
+            // The method returns -1 if character or string is not found.
+            Console.WriteLine("IndexOf and LastIndexOf Method");
+            Console.WriteLine(s2.IndexOf("I"));     // 0
+            Console.WriteLine(s2.IndexOf("y"));     // 23
+            Console.WriteLine(s2.LastIndexOf("i")); // 26
+            Console.WriteLine(s2.IndexOf("a"));     // 4
+            Console.WriteLine(s2.LastIndexOf("a")); // 14
+            Console.WriteLine(s2.IndexOf("b"));     // -1
+            Console.WriteLine(s2.IndexOf("c"));     // -1
+            // <IndexOf LastIndexOf/>
 
+            // </Compare and IndexOf>
+            // Let's combine the Contains and IndexOf Methods in a quick program. 
+            Console.WriteLine("Contains and IndexOf Methods Combined");
+            
+            string searchFor = "aunt";
+            bool b = s1.Contains(searchFor);
+            Console.WriteLine($"{searchFor} is in the string {s1}: {b}");
+           
+            if (b) {
+                int index = s1.IndexOf(searchFor);
+                if (index >= 0) {
+                Console.WriteLine($"{searchFor} begins at character position {index + 1}");       
+                }
+            }
+            // <Compare and IndexOf/>
+
+            
         }
     }
 }
